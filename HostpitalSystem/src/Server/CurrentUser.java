b9457b95144d9377db0ebfcf72f6cb9fcdd69656
Johan <tojohansvensson@gmail.com>
2014-02-16 02:02:42 +0100

    Jobbar på att hämta ut Patienter ur listan

---
+++ HostpitalSystem/src/Server/CurrentUser.java
@@ -0,0 +1,73 @@
+package Server;
+
+
+/**
+ * CurrentUser represents the current user that has logged on to
+ * the movie booking system. It is a singleton class.
+ */
+public class CurrentUser {
+    /**
+     * The single instance of this class
+     */
+    private static CurrentUser instance;
+        
+    /**
+     * The name of the current user.
+     */
+    private String currentPnbr;
+    
+    private int currentLevel;
+        
+    /**
+     * Create a CurrentUser object.
+     */
+    private CurrentUser() {
+        currentPnbr = null;
+        currentLevel = 0;
+    }
+        
+    /**
+     * Returns the single instance of this class.
+     *
+     * @return The single instance of the class.
+     */
+    public static CurrentUser instance() {
+        if (instance == null)
+            instance = new CurrentUser();
+        return instance;
+    }
+        
+    /**
+     * Check if a user has logged in.
+     *
+     * @return true if a user has logged in, false otherwise.
+     */
+    public boolean isLoggedIn() {
+        return currentPnbr != null;
+    }
+        
+    /** 
+     * Get the user id of the current user. Should only be called if
+     * a user has logged in.
+     *
+     * @return The user id of the current user.
+     */
+    public String getCurrentUserId() {
+        return currentPnbr == null ? "<none>" : currentPnbr;
+    }
+        
+    /**
+     * A new user logs in.
+     *
+     * @param userId The user id of the new user.
+     */
+    public void loginAs(String userId, int level) {
+        currentPnbr = userId;
+        currentLevel = level;
+    }
+    
+    public int getCurrentLevel(){
+    	return currentLevel == 0 ? 0 : currentLevel;
+    }
+}
+   
--- HostpitalSystem/src/Server/PatientList.java
+++ HostpitalSystem/src/Server/PatientList.java
@@ -4,21 +4,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PatientList {
-	HashMap<String,Patient> allPatient;
-	
-	public PatientList(){
+	HashMap<String, Patient> allPatient;
+
+	public PatientList() {
 		allPatient = new HashMap<String, Patient>();
 	}
-	
-	public void addPatient(Patient p){
+
+	public void addPatient(Patient p) {
 		allPatient.put(p.getPnbr(), p);
 	}
-	
-	public Patient getPatient(String patientPnbr){
+
+	public Patient getPatient(String patientPnbr) {
 		Patient p = allPatient.get(patientPnbr);
-		if(p == null) return p;
-		// Here you must put in the doctors Pnbr in the isTreatedBy-sats.
-		if( p.isTreatedBy(pnbr) ) return p;
+		if (p == null)
+			return p;
+		if (p.isTreatedBy(CurrentUser.instance().getCurrentUserId()))
+			return p;
+		else {
+			return null;
 		}
-	
+	}
+
 }
